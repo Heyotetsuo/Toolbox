3c8961c9f5f8691a7c6a220356f1c2db025880eb
Manny Morales <manny@movingartwork.net>
2019-08-10 20:36:50 -0400

    progress on cbkts.py

--- refactor/cbkts.py
+++ refactor/cbkts.py
@@ -1,21 +1,25 @@
-#!/data/data/com.termux/files/usr/bin/python3
+#!/usr/bin/env python3
 
 import sys
-import os
 import re
 
 fname1 = str( sys.argv[1] )
 fname2 = str( sys.argv[2] )
-f1 = open( str(fname1), "r", encoding="utf8" )
-f2 = open( str(fname2), "w", encoding="utf8" )
+fname3 = '/mnt/c/Users/Lori/Desktop/pythonlog.txt'
+f1 = open( str(fname1), 'r', encoding='utf8' )
+f2 = open( str(fname2), 'w', encoding='utf8' )
+# f3 = open( fname3, 'w', encoding='utf8' )
 
-rgx = r'\)\s*\{\s*$'
-re.compile( rgx, re.MULTILINE )
+rgxS = r'\)\s*\{\s*$'
+rgxO = re.compile( rgxS, re.MULTILINE )
 
 s = f1.read()
-while ( re.search( rgx, s )):
-  s = rgx.replace( s, '\r{' )
+# f3.write( str(re.search(rgx,s)) + '\n' )
+# while ( re.search( rgx, s )):
+s = re.sub( rgxO, ')\n{', s )
 
-f2.write(s)
+f2.write( s )
+
 f1.close();
 f2.close();
+# f3.close();
