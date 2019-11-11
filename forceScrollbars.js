93f6850fdcb8085f9a6a25c44d4a09a7f1419ce5
Manny Morales <manny@movingartwork.net>
2019-11-11 13:55:32 -0500

    Improved reliability

--- forceScrollbars.js
+++ forceScrollbars.js
@@ -2,13 +2,16 @@
 // To use, simply paste this code into the browser's console and press return
 (function(){
 	var all = document.all;
+	var style = {};
 	var i = 0;
 	while ( i < all.length ){
-		if (
-			all[i].style &&
-			all[i].style.overflow
-		){
-			all[i].style.overflow = null;
+		if ( style.overflow !== "" )
+		{
+			all[i].style.cssText = "overflow: visible !important";
+		}
+		if ( style.overflowY !== "" )
+		{
+			all[i].style.cssText = "overflow-y: visible !important";
 		}
 		i += 1;
 	}
