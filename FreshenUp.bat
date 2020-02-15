d08fd1fc9bdd06a56d563eb0b15d2353dfa5b10d
Manny <manny@movingartwork.net>
2020-02-15 16:40:16 -0500

    Improved killing of Adobe, Skype, and Updates

--- FreshenUp.bat
+++ FreshenUp.bat
@@ -1,18 +1,18 @@
+wmic service where 'name like "%%Adobe%%"' call stopservice
 wmic service where 'name like "%%Update%%"' call stopservice
+wmic service where 'name like "%%UsoSvc%%"' call stopservice
+wmic process where 'name like "%%Adobe%%"' call terminate
+wmic process where 'name like "%%Skype%%"' call terminate
+wmic process where 'name like "%%UsoClient%%"' call terminate
+wmic process where 'name like "%%Microsoft%%"' call terminate
 sc stop AGMService
 sc stop AGSService
 sc stop TabletInputService
-sc stop AdobeARMservice
 sc stop BTAGService
 sc stop bthserv
 sc stop wuauserv
-taskkill -t -f -im Adobe*
 taskkill -t -f -im Creative*
-taskkill -t -f -im Acro*
-taskkill -t -f -im acro*
-taskkill -t -f -im Skype*
 taskkill -t -f -im ctfmon*
-taskkill -t -f -im Microsoft.Photos*
 taskkill -t -f -im OneDrive*
 taskkill -t -f -im WinStore*
 taskkill -t -f -im rundll*
